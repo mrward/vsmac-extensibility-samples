05d907b19b9e13a2ca4f90dbcd0d0db669822efd
Matt Ward <matt.ward@microsoft.com>
2022-10-15 14:04:22 +0100

    Add startup handler sample

---
+++ StartupHandler/README.md
@@ -0,0 +1,4 @@
+# Startup Handler sample extension
+
+This sample shows how to have code run when Visual Studio for Mac is started
+by implementing a startup handler.
---
+++ StartupHandler/src/Properties/AddinInfo.cs
@@ -0,0 +1,10 @@
+﻿using Mono.Addins;
+
+[assembly: Addin(
+    "StartupHandlerSample",
+    Namespace = "Microsoft.VisualStudioMac.Extensibility.Samples",
+    Version = "0.1",
+    Category = "IDE extensions")]
+
+[assembly: AddinName("Startup Handler Sample")]
+[assembly: AddinDescription("Extension that has a startup handler")]
